a9a0cf6afaafe792614bddfa488e88713612b764
Lejian Luo <luolejian@outlook.com>
2025-02-17 23:35:56 +1300

    state only update when inspect element attempt fix no.4

--- components/ProductInput.jsx
+++ components/ProductInput.jsx
@@ -64,7 +64,7 @@ export default function ProductInput({output,setOutput,outputPrompts,setPrompts}
 
     return(<>
         <div className='flex justify-center'>
-            <input type='text' className='"w-150 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"' onChange={(e)=>setProduct(e.target.value)}/>
+            <input type='text' className='"w-150 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"' onInput={(e)=>setProduct(e.target.value)}/>
             <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' onClick={()=>analyzeImageRequest(product)}>分析商品</button>
             {product?<></>:<p>empty</p>}
         </div>
